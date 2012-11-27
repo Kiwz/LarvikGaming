a731fcdb4fa4a836cfe719b75c5292af1cdf27dc
Kiwz <tho-ask@online.no>
2012-11-27 09:05:12 +0100

    Changed tablist.color perm nodes to larvikgaming.tab.color

--- src/net/kiwz/larvikgaming/listeners/LoginListener.java
+++ src/net/kiwz/larvikgaming/listeners/LoginListener.java
@@ -12,49 +12,49 @@ public class LoginListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player p = event.getPlayer();
 
-        if (p.hasPermission("tablist.aqua")) {
+        if (p.hasPermission("larvikgaming.tab.aqua")) {
             p.setPlayerListName(ChatColor.AQUA + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.black")) {
+        else if (p.hasPermission("larvikgaming.tab.black")) {
             p.setPlayerListName(ChatColor.BLACK + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.blue")) {
+        else if (p.hasPermission("larvikgaming.tab.blue")) {
             p.setPlayerListName(ChatColor.BLUE + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.dark_aqua")) {
+        else if (p.hasPermission("larvikgaming.tab.dark_aqua")) {
             p.setPlayerListName(ChatColor.DARK_AQUA + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.dark_blue")) {
+        else if (p.hasPermission("larvikgaming.tab.dark_blue")) {
             p.setPlayerListName(ChatColor.DARK_BLUE + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.dark_gray")) {
+        else if (p.hasPermission("larvikgaming.tab.dark_gray")) {
             p.setPlayerListName(ChatColor.DARK_GRAY + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.dark_green")) {
+        else if (p.hasPermission("larvikgaming.tab.dark_green")) {
             p.setPlayerListName(ChatColor.DARK_GREEN + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.dark_purple")) {
+        else if (p.hasPermission("larvikgaming.tab.dark_purple")) {
             p.setPlayerListName(ChatColor.DARK_PURPLE + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.dark_red")) {
+        else if (p.hasPermission("larvikgaming.tab.dark_red")) {
             p.setPlayerListName(ChatColor.DARK_RED + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.gold")) {
+        else if (p.hasPermission("larvikgaming.tab.gold")) {
             p.setPlayerListName(ChatColor.GOLD + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.gray")) {
+        else if (p.hasPermission("larvikgaming.tab.gray")) {
             p.setPlayerListName(ChatColor.GRAY + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.green")) {
+        else if (p.hasPermission("larvikgaming.tab.green")) {
             p.setPlayerListName(ChatColor.GREEN + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.light_purple")) {
+        else if (p.hasPermission("larvikgaming.tab.light_purple")) {
             p.setPlayerListName(ChatColor.LIGHT_PURPLE + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.red") || p.hasPermission("tablist.*")) {
+        else if (p.hasPermission("larvikgaming.tab.red") || p.hasPermission("larvikgaming.tab.*")) {
             p.setPlayerListName(ChatColor.RED + p.getName() + ChatColor.WHITE);
         }
-        else if (p.hasPermission("tablist.yellow")) {
+        else if (p.hasPermission("larvikgaming.tab.yellow")) {
             p.setPlayerListName(ChatColor.YELLOW + p.getName() + ChatColor.WHITE);
         }
     }
